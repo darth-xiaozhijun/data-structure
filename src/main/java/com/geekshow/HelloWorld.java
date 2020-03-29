a74b7f8aeaab6f4b93725b8daf1120d9b5a70541
肖智军 <@LAPTOP-5A01DPQ1>
2020-03-29 23:47:26 +0800

    初始化项目

---
+++ src/main/java/com/geekshow/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.geekshow;
+
+/**
+ * 
+ * @author Ð¤ÖÇ¾ü
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
